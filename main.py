5cea8ea10441c235226b847bd0aeaa6f67660ef1
mtysac <mtysabelcab@gmail.com>
2025-11-06 13:17:05 -0330

    Screen updated and assets folder added

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ import pygame
 
 pygame.init()
 
-ICON_PATH = "assets/images/player.png"
-game_icon = pygame.image.load(os.path.join(ICON_PATH)) 
-pygame.display.set_icon(game_icon)
+# ICON_PATH = "assets/images/icon.png"
+# game_icon = pygame.image.load(os.path.join(ICON_PATH))
+# pygame.display.set_icon(game_icon)
 SCREEN = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+
+def main():
+    while True:
+        pygame.display.set_caption("My Pygame Window")
+        SCREEN.fill((158, 191, 219))
+
+        MOUSE_POS = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit
+                    sys.exit()
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
