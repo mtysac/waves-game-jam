fef168e6ef463eb5d38d813ca27e1cbd0ce80718
mtysac <mtysabelcab@gmail.com>
2025-11-07 22:29:39 -0330

    Clouds and Button scripts added

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import pygame
 
 pygame.init()
 
-# ICON_PATH = "assets/images/icon.png"
-# game_icon = pygame.image.load(os.path.join(ICON_PATH))
-# pygame.display.set_icon(game_icon)
+# window setup
+#ICON_PATH = "assets/images/icon.png"
+#game_icon = pygame.image.load(os.path.join(ICON_PATH))
+#pygame.display.set_icon(game_icon)
 SCREEN = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 
 def main():
+    # audio
+    #pygame.mixer.music.load('data/startup.mp3')
+    #pygame.mixer.music.set_volume(0.6)
+    #pygame.mixer.music.play(-1) # -1 means loop forever
+
     while True:
+        # display
         pygame.display.set_caption("My Pygame Window")
         SCREEN.fill((158, 191, 219))
 
